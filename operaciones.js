08cef25219fb44dbe92e09ba737c670af2c669b3
felipe benitez <cachaofpower@g,ail.com>
2022-10-30 01:15:17 -0500

    primeros pasos

---
+++ operaciones.js
@@ -0,0 +1,16 @@
+var numero1 = 3;
+var numero2 = 2;
+var suma = 0;
+var resta = 0;
+var mult = 1;
+var div = 0;
+suma = suma + numero1 + numero2;
+resta = numero1 - numero2;
+mult = numero1 * numero2;
+div = numero1/numero2
+
+
+console.log(suma);
+console.log(resta);
+console.log(mult);
+console.log(div);
